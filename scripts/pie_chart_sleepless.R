3ac0ab85b114cdebc9fd1e7542ef6fe75119c9a9
Cynthia Yeh <yehc2@uw.edu>
2020-05-21 14:37:23 -0700

    lintr error

--- scripts/pie_chart_sleepless.R
+++ scripts/pie_chart_sleepless.R
@@ -7,17 +7,23 @@ library("leaflet")
 library("plotly")
 
 
-datatable <- read.csv("Time_Americans_Spend_Sleeping.csv", stringsAsFactors = FALSE)
+datatable <- read.csv("Time_Americans_Spend_Sleeping.csv",
+  stringsAsFactors = FALSE
+)
 View(datatable)
 
 
 all_days <- datatable %>% filter(Type.of.Days == "All days")
-ggplot(data = all_days, aes(x=Type.of.Days, fill = Age.Group)) +
+ggplot(data = all_days, aes(x = Type.of.Days, fill = Age.Group)) +
   geom_bar(width = 1) +
-  coord_polar(theta ="y")
+  coord_polar(theta = "y")
 
 nonholidays <- datatable %>% filter(Type.of.Days == "Nonholiday weekdays")
-ggplot(data = nonholidays, aes(x=Type.of.Days, fill = Age.Group)) +
+ggplot(data = nonholidays, aes(x = Type.of.Days, fill = Age.Group)) +
   geom_bar(width = 1) +
-  coord_polar(theta ="y")
+  coord_polar(theta = "y")
 
+weekends <- datatable %>% filter(Type.of.Days == "Weekend days and holidays")
+ggplot(data = weekends, aes(x = Type.of.Days, fill = Age.Group)) +
+  geom_bar(width = 1) +
+  coord_polar(theta = "y")
