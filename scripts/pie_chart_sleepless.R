f8db790b5f93d73df42fd304281049fc8dc89211
Cynthia Yeh <yehc2@uw.edu>
2020-05-20 19:56:54 -0700

    pie charts update

--- scripts/pie_chart_sleepless.R
+++ scripts/pie_chart_sleepless.R
@@ -1,11 +1,23 @@
 # Pie Chart
 install.packages("ggplot2")
-library("ggplot2")
 library("dplyr")
+library("ggplot2")
+library("stringr")
+library("leaflet")
+library("plotly")
 
-dt <- read.csv("data/sleeepless in america/Time Americans Spend Sleeping.csv")
+
+datatable <- read.csv("Time_Americans_Spend_Sleeping.csv", stringsAsFactors = FALSE)
+View(datatable)
 
 
-ggplot(data = dt, aes(x = factor(1), fill = factor(cyl))) +
+all_days <- datatable %>% filter(Type.of.Days == "All days")
+ggplot(data = all_days, aes(x=Type.of.Days, fill = Age.Group)) +
   geom_bar(width = 1) +
-  coord_polar(theta = "y")
+  coord_polar(theta ="y")
+
+nonholidays <- datatable %>% filter(Type.of.Days == "Nonholiday weekdays")
+ggplot(data = nonholidays, aes(x=Type.of.Days, fill = Age.Group)) +
+  geom_bar(width = 1) +
+  coord_polar(theta ="y")
+
