32916d87ff79c469465294392595548af443d050
ryan-du <ryan.du2009@gmail.com>
2020-05-21 02:10:04 -0700

    finish table of summary

--- scripts/table_summary_sleepless.R
+++ scripts/table_summary_sleepless.R
@@ -1,1 +1,17 @@
-table of summary information
+#table of summary information
+
+library(dplyr)
+
+
+time_americans_sleep <- read.csv("Time_Americans_Spend_Sleeping.csv", stringsAsFactors = FALSE)
+
+
+data_table <- time_americans_sleep %>%
+  select(Year, Sex, Age.Group, Average.sleep = Avg.hrs.per.day.sleeping, Type.of.Days) %>%
+  group_by(Age.Group)
+
+
+
+
+
+
