ff83cfe0cd72d872826bbe9d4dc9594172639b5e
Cynthia Yeh <yehc2@uw.edu>
2020-05-20 19:03:23 -0700

    change file name

--- scripts/pie_chart_sleepless.R
+++ scripts/pie_chart_sleepless.R
@@ -1,1 +1,11 @@
-Pie Chart
+# Pie Chart
+install.packages("ggplot2")
+library("ggplot2")
+library("dplyr")
+
+dt <- read.csv("data/sleeepless in america/Time Americans Spend Sleeping.csv")
+
+
+ggplot(data = dt, aes(x = factor(1), fill = factor(cyl))) +
+  geom_bar(width = 1) +
+  coord_polar(theta = "y")
